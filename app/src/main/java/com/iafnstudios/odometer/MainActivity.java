17d7017c7025dafba4becaa02cbfa663f6f1295c
sen-abdullah@yandex.com <aa1991aa>
2018-03-02 19:33:47 +0300

    First version of app

---
+++ app/src/main/java/com/iafnstudios/odometer/MainActivity.java
@@ -0,0 +1,72 @@
+package com.iafnstudios.odometer;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private OdometerService odometerService;
+    private boolean bound = false;
+
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            OdometerService.OdometerBinder odometerBinder = (OdometerService.OdometerBinder) service;
+            odometerService = odometerBinder.getOdometer();
+            bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bound = false;
+        }
+    };
+
+    private void watchMileage(){
+        final TextView distanceView = (TextView) findViewById(R.id.distance);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                double distance = 0.0;
+                if (odometerService != null){
+                    distance = odometerService.getMiles();
+                }
+                String distanceStr = String.format("%1$,.2f miles",distance);
+                distanceView.setText(distanceStr);
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        watchMileage();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this,OdometerService.class);
+        bindService(intent,connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (bound){
+            unbindService(connection);
+            bound=false;
+        }
+    }
+}
